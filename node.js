70b26043ceb8fc279f28c7feddf6d79ca3a5baf2
Anthony Cassaigne <anthony.cassaigne@gmail.com>
2013-11-29 22:24:54 +0100

    begin doc on node.js

---
+++ node.js
@@ -0,0 +1,10 @@
+*******
+node.js
+*******
+
+
+Resource
+- TDD http://webapplog.com/test-driven-development-in-node-js-with-mocha/
+- http://developpementagile.com/posts/2013/may/tests-javascript-avec-jasmine-partie-1
+- http://fr.openclassrooms.com/informatique/cours/des-applications-ultra-rapides-avec-node-js
+
