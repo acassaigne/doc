85193bff265017981c347a9bd8112cdf0878b22e
Anthony Cassaigne <anthony.cassaigne@gmail.com>
2013-11-29 23:20:40 +0100

    Add resource on express

--- node.js
+++ node.js
@@ -7,4 +7,8 @@ Resource
 - TDD http://webapplog.com/test-driven-development-in-node-js-with-mocha/
 - http://developpementagile.com/posts/2013/may/tests-javascript-avec-jasmine-partie-1
 - http://fr.openclassrooms.com/informatique/cours/des-applications-ultra-rapides-avec-node-js
-
+- http://www.js-attitude.fr/node-js/
+- http://net.tutsplus.com/tutorials/javascript-ajax/introduction-to-express/
+- http://stackoverflow.com/questions/8144214/learning-express-for-node-js
+- http://shapeshed.com/creating-a-basic-site-with-node-and-express/
+- http://www.atinux.fr/2011/08/31/introduction-a-express-js-partie-1-la-base/
