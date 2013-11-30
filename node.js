e1d12b342f454fb07a131d2f79cc10f020fe86c4
Anthony Cassaigne <anthony.cassaigne@gmail.com>
2013-11-30 18:08:16 +0100

    Resource on jade

--- node.js
+++ node.js
@@ -11,4 +11,14 @@ Resource
 - http://net.tutsplus.com/tutorials/javascript-ajax/introduction-to-express/
 - http://stackoverflow.com/questions/8144214/learning-express-for-node-js
 - http://shapeshed.com/creating-a-basic-site-with-node-and-express/
-- http://www.atinux.fr/2011/08/31/introduction-a-express-js-partie-1-la-base/
+- http://www.atinux.fr/2011/08/31/introduction-a-express-js-partie-1-la-base/b
+
+
+Moteur de template
+==================
+
+- http://code4fun.fr/creer-un-site-simple-avec-node-js-express-et-jade/
+- http://www.franz-enzenhofer.com/jade
+- http://naltatis.github.io/jade-syntax-docs/
+- https://github.com/visionmedia/jade
+- https://github.com/primaryobjects/Node.js-Bootstrap-Starter-Template
